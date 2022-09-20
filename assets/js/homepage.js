b3b5834fd873fe19fdcffaef51d13ec5edce2f48
alexfortin <fortin.alex14@gmail.com>
2022-09-19 20:19:38 -0500

    made final changes

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,6 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getUserRepos = function (user) {
     //format the githib api url
@@ -92,4 +93,34 @@ var displayRepos = function (repos, searchTerm) {
 
 }
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues"
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language)
+            })
+        }
+        else {
+            alert("Error: GitHub user not found")
+        }
+    })
+}
+
+var buttonClickHandler = function(event) {
+
+    var language = event.target.getAttribute("data-language")
+    console.log(language)
+
+    if (language) {
+        getFeaturedRepos(language)
+
+        //clear old content
+        repoContainerEl.textContent = ""
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler)
